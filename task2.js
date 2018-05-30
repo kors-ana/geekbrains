070ed433c1e9185e4aa77acb1e5e170416e68de7
Korsikova <nastcka@gmail.com>
2018-05-30 17:42:39 +0300

    changed input

--- task2.js
+++ task2.js
@@ -1,8 +1,8 @@
 var m = { "января": 0, "февраля": 1, "марта": 2, "апреля": 3, "мая": 4, "июня": 5, "июля": 6, "августа": 7, "сентября": 8, "октября": 9, "ноября": 10, "декабря": 11 };
 
-var day = process.argv[2];
-var month = m[process.argv[3]];
-var year = process.argv[4];
+var day = process.argv[1];
+var month = m[process.argv[2]];
+var year = process.argv[3];
 
 var newYearDate = new Date(year, 11, 31);
 var nowDate = new Date(year, month, day);
