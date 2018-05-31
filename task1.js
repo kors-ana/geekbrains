28445834586953f8fb033db8440170918e42c42d
Korsikova <nastcka@gmail.com>
2018-05-31 13:06:47 +0300

    changed input condition

--- task1.js
+++ task1.js
@@ -2,12 +2,16 @@ var number = Number(process.argv[1]);
 var numberSystem = Number(process.argv[2]);
 var requiredNumberSystem = Number(process.argv[3]);
 
-
-for (var i = 0; i >= 0; i++) {
-    if (i.toString(numberSystem) == number) {
-        var res = i.toString(requiredNumberSystem);
-        res = String(res);
-        process.stdout.write(res);
-        break;
-    } 
-} 
+if (number >=0 && number <= 10000 && numberSystem >=2 && requiredNumberSystem <= 36) {
+    for (var i = 0; i >= 0; i++) {
+        if (i.toString(numberSystem) == number) {
+            var res = i.toString(requiredNumberSystem);
+            res = String(res);
+            process.stdout.write(res);
+            break;
+        } 
+    }
+} else {
+    throw new Error ('Range Error');
+}
+ 
